9dfdd47ddf588a9115809bd0abeef4b12304df11
filletournesols <dgalarcegonzalez@gmail.com>
2023-03-06 20:07:31 -0300

    una cosa poca de la HU2 (solo html y css)

--- src/Components/TakeOrders/TakeOrders.jsx
+++ src/Components/TakeOrders/TakeOrders.jsx
@@ -1,20 +1,38 @@
 import { Form } from 'react-router-dom'
 
 import Btn from '../ReusableComponents/Btn'
-// import logout from '../../util/auth'
+import Logo from '../ReusableComponents/Logo'
+import './TakeOrders.css'
 
 const TakeOrders = () => {
-    // cambiar nombre clase botón
-    const btn = 'log_in_btn'
-    // const btnType = 'button'
-    // btnType={btnType} 
+    const logo = 'take_orders_logo'
+    const logOutBtn = 'log_out_btn'
+    const ordersBtn = 'orders_btn'
+    const newOrderBtn = 'new_order_btn'
+    const breakfastBtn = 'breakfast_btn'
+    const drinksBtn = 'drinks_btn'
+    const regularBtn = 'regular_btn'
 
     return (
-        <div>
-            <h1>PARA TODO EL DETALLE DE LAS ÓRDENES</h1>
-            <Form action='/logout' method='post'>
-            <Btn cssBtn={btn} text={'Cerrar sesión'}></Btn>
-            </Form>
+        // <div>
+            <div className='take_orders'>
+                <Logo cssLogo={logo}></Logo>
+            {/* </div> */}
+            {/* <div className='take_orders_btns'> */}
+                <Form action='/logout' method='post' className='form'>
+                    <Btn cssBtn={logOutBtn} text={'Cerrar sesión'}></Btn>
+                    <label className='user_name'>NOMBRE</label>
+                </Form>
+                <div className='orders_btn_list'>
+                    <Btn cssBtn={ordersBtn} text={'Órdenes'}></Btn>
+                    <Btn cssBtn={newOrderBtn} text={'Nueva orden'}></Btn>
+                </div>
+                <div className='order_options'>
+                    <Btn cssBtn={drinksBtn} text={'Bebestibles'}></Btn>
+                    <Btn cssBtn={breakfastBtn} text={'Desayuno'}></Btn>
+                    <Btn cssBtn={regularBtn} text={'Regular'}></Btn>
+                </div>
+            {/* </div> */}
         </div>
     );
 }
