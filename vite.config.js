164a8247a0aad9a34e9b493d65c783ff6050ed73
filletournesols <dgalarcegonzalez@gmail.com>
2023-03-21 16:57:36 -0300

    arreglos netlify

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/chezMimi/',
   plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setuptest.js'
-  }
+  },
+  target: 'serverless'
 })
